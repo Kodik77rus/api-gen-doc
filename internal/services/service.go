997b6ca0c2fdce8f9ababcf3476ba9ed570c71c3
Kodik77rus <plyhach@gmail.com>
2022-06-07 14:19:49 +0300

    add http validation

--- internal/services/service.go
+++ internal/services/service.go
@@ -3,28 +3,31 @@ package services
 import (
 	"io/ioutil"
 	"net/http"
+	url2 "net/url"
 )
 
 func HttpResponse(url string) (string, error) {
+	u, err := url2.Parse(url)
+	if err != nil {
+		return "", err
+	}
+
 	c := http.Client{}
 
-	req, err := http.NewRequest("GET", url, nil)
-
+	req, err := http.NewRequest("GET", u.String(), nil)
 	if err != nil {
 		return "", nil
 	}
 
 	req.Header.Add("User-Agent", "hackerman")
-	resp, err := c.Do(req)
 
+	resp, err := c.Do(req)
 	if err != nil {
 		return "", nil
 	}
-
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
-
 	if err != nil {
 		return "", nil
 	}
